ba17bd5216a5b5eaec33200f3fbf18da8287d97d
zhonglaogen <ZLXzlxxs98@163.com>
2020-03-11 20:43:25 +0800

    makefile

---
+++ makefile/e2/test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "tool.h"
+
+int main(){
+  printf("hello word\n");
+  sayHello();
+  printf("a\n");
+  return 0;
+}
---
+++ makefile/e2/tool.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include "tool.h"
+
+void sayHello(){
+  printf("sayhello/n");
+}
